9e8c5ef1af9ad659ea5ced12fa918b7cc9d32010
ryukkn <126628758+ryukkn@users.noreply.github.com>
2024-04-03 21:30:14 +0800

    sad

--- data.sql
+++ data.sql
@@ -5,7 +5,13 @@ INSERT INTO `students` (`ID`, `email`, `password`,`contact`,`fullname`, `QR`)
     VALUES ('SF23sSCZDA1@ls', 'test@gmail.com', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', '09957140344', 'Test', 's-test');
 
 INSERT INTO `admin` (`ID`, `email`, `password`,`contact`, `fullname`) 
-    VALUES ('SFasdACZDA1@ls', 'admin@gmail.com', 'd033e22ae348aeb5660fc2140aec35850c4da997', '09999231412', 'Admin' );
+    VALUES ('SFasdACZDA1@ls', 'superadmin@gmail.com', 'd033e22ae348aeb5660fc2140aec35850c4da997', '09999231412', 'Superadmin' );
+
+INSERT INTO `admin` (`ID`, `email`, `password`,`contact`, `fullname`) 
+    VALUES ('SFasdACZDA2@ls', 'admin1@gmail.com', 'd033e22ae348aeb5660fc2140aec35850c4da997', '09999231412', 'Admin' );
+
+INSERT INTO `admin` (`ID`, `email`, `password`,`contact`, `fullname`) 
+    VALUES ('SFasdACZDA3@ls', 'admin2@gmail.com', 'd033e22ae348aeb5660fc2140aec35850c4da997', '09999231412', 'Admin' );
 
 INSERT INTO `faculty` (`ID`, `email`, `password`,`contact`,`fullname`) 
     VALUES ('SF23sSCZAA1@ls', 'test@gmail.com', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', '09957140344', 'Faculty');
--- lib/server/connection.dart
+++ lib/server/connection.dart
@@ -1,4 +1,5 @@
 class Connection{
+  // http://<address>:<port>
   static const _address = "https://ryuk-tests.000webhostapp.com/";
   static const host = _address;
   static const socket = "ws://$_address";
